8ce255a35d473417ae4ec19fdb5e48020e0ac011
mariamiren <mcarmona8@uoc.edu>
2025-12-03 18:59:00 +0100

    Update ej2c1.py

--- 2c/ej2c1.py
+++ 2c/ej2c1.py
@@ -49,8 +49,12 @@ Exemple:
 """
 
 
-def convert_to_integer(string):
-    # Write here your code
+def convert_to_integer():
+    assert convert_to_integer("10") == 10 "convert_to_integer does not return the correct value for input '10'. It should be 10"
+    assert convert_to_integer("-10") == -10 "convert_to_integer does not return the correct value for input '-10'. It should be -10"
+    assert convert_to_integer("10.5") == "The string cannot be converted to an integer", "convert_to_integer does not return the correct value for input '10.5'. It should be 'The string cannot be converted to an integer'"
+    assert convert_to_integer("abc") == "The string cannot be converted to an integer", "convert_to_integer does not return the correct value for input 'abc'. It should be 'The string cannot be converted to an integer'"
+    assert convert_to_integer("5").__contains__("An unexpected error has ocurred:"), "convert _to_integer does not return the correct value for input['5']. It should be 'An unexpected error has ocurred:'"
     pass
 
 
